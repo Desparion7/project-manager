53e046535679cbd061fa9ee6d0d7b94873fdb325
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2024-05-14 20:15:57 +0200

    change layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
 import './globals.css';
 import { siteConfig } from '@/config/site';
 
@@ -22,8 +23,10 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang='en'>
-			<body className={inter.className}>{children}</body>
-		</html>
+		<ClerkProvider>
+			<html lang='en'>
+				<body className={inter.className}>{children}</body>
+			</html>
+		</ClerkProvider>
 	);
 }
